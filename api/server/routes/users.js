fc5279704aeb9557f8f1994bedb771c34d303779
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2020-03-08 19:20:34 -0700

    add CRUD routes for users with unit tests

--- api/server/routes/users.js
+++ api/server/routes/users.js
@@ -3,32 +3,57 @@ const knex = require('../../db/config')
 
 module.exports = (app) => {
     //index
+    // For debug only!!!
+    // *****************
+    // Never expose all the users like below in a production application!!!
     app.get('/users', (req, res) => {
             knex.select().table('users')
               .then(data => res.status(200).send(data))
               .catch(err => res.status(500).send(err));
         }
     )
+
     //show
+    app.get('/users/:userId', (req, res) => {
+            //console.log(req.params)
+            let id = parseInt(req.params.userId)
+
+            knex.select().table('users')
+              .where('userId', id)
+              .then(data => res.status(200).send(data))
+              .catch(err => res.status(500).send(err));
+        }
+    )
     
+    //create 
+    app.post('/users', (req, res) => {
+        //console.log(req.body)
+        //console logging here shows req object when you run the tests
+        knex('users')
+            .insert(req.body)
+            .then(() => res.status(201).send())
+            .catch(err => res.status(500).send(err));
+    })
+
+    //update
+    app.put('/users/:userId', (req, res) => {
+        knex('users')
+            .update(req.body.data, Object.keys(req.body.data))
+            .then(() => res.status(200).send())
+            .catch(err => res.status(500).send(err));
+    })
+
+    //delete
+    app.delete('/users/:userId', (req,res) => {
+        console.log(req.params)
+        let id = parseInt(req.params.userId)
+
+        console.log('id', id)
+        knex('users')
+            .where('userId', id)
+            .del()
+            .then(() => res.status(200).send())
+            .catch(err => res.status(500).send(err));
+    })
 }
-
-
-
-// For debug only!!!
-// *****************
-// Never expose all the users like below in a production application!!!
-
-// const getUsers = (req, res) => {
-//     knex.select().table('users')
-    
-//       .then(data => res.status(200).send(data))
-//       .catch(err => res.status(500).send(err));
-// }; 
-
-//const getUser =
-
-// module.exports = {
-//     getUsers
-// }
 
--- api/tests/users.test.js
+++ api/tests/users.test.js
@@ -5,6 +5,12 @@ const knex = require('../db/config');
 
 process.env.NODE_ENV = 'test';
 
+//test setup
+const newUser = {
+    first_name: 'Tina',
+    last_name: 'Belcher'
+}
+
 beforeEach(()=>{
     return knex.migrate.rollback()
       .then(() => knex.migrate.latest())
@@ -19,6 +25,7 @@ afterAll(() => {
     knex.destroy()
 })
 
+//move to static pages test file
 describe('Get root path', ()=>{
     test('root path should respond with 200', async () =>{
         const response = await request(app).get('/');
@@ -26,9 +33,64 @@ describe('Get root path', ()=>{
     })
 });
 
+//=====================================
+//test user CRUD routes
+//=====================================
+//index
 describe('get /users', ()=>{
     test('Get /users should respond with 200', async () =>{
         const response = await request(app).get('/users');
         expect(response.statusCode).toBe(200);
     })
 });
+
+//show
+describe('get users with userId of 1', ()=>{
+    test('Get /users should respond with 200', async () =>{
+        const response = await request(app).get('/users/1')
+        expect(response.statusCode).toBe(200);
+
+    })
+});
+
+//create 
+describe('POST /user', ()=>{
+    test('It should respond with a 201', async () =>{
+        const response = await request(app)
+            .post('/users')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(newUser));
+
+        expect(response.statusCode).toBe(201);
+    })
+});
+
+//update
+describe('PUT /user', () => {
+    test('It should respond with 200', async () => {
+        const response = await request(app)
+            .put('/users/1')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send({ data: { first_name: 'Mitchellupdated' } })
+
+        expect(response.statusCode).toBe(200);
+    })
+})
+
+describe("DELETE /user/:userId", () => {
+    test('It should respond with 200', async () => {
+        const response = await request(app)
+            .delete('/users/1')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+        
+        expect(response.statusCode).toBe(200);
+    })
+})
+
+
+//new 
+//edit
+//destroy
